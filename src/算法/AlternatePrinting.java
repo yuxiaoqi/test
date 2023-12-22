d4740e7fb04ded69bfbd3f9ca9edde3ffb26ff05
1981573744@qq.com <yuyanqi520>
2023-12-22 18:24:17 +0800

    算法交替输出12121212

---
+++ src/算法/AlternatePrinting.java
@@ -0,0 +1,32 @@
+package 算法;
+
+public class AlternatePrinting {
+    private static final Object lock = new Object();
+    private static boolean isOne = false;
+
+    public static void main(String[] args) {
+       // Thread tt =new Thread(()->printNumbers(3));
+        Thread thread1 = new Thread(() -> printNumbers(1));
+        Thread thread2 = new Thread(() -> printNumbers(2));
+
+        thread1.start();
+        thread2.start();
+    }
+
+    private static void printNumbers(int number) {
+        for (int i = 0; i < 5; i++) {
+            synchronized (lock) {
+                try {
+                    while ((number == 1 && !isOne) || (number == 2 && isOne)) {
+                        lock.wait();
+                    }
+                    System.out.print(number);
+                    isOne = !isOne;
+                    lock.notifyAll();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
---
+++ src/算法/AlternatePrintingA.java
@@ -0,0 +1,62 @@
+package 算法;
+
+/**
+ * @ClassName:
+ * @Description:
+ * @Author yuxiaoqi
+ * @Create 2023/12/22 17:28
+ */
+public class AlternatePrintingA {
+
+    //用户锁交互
+    private static final Object lock = new Object();
+
+    private static volatile boolean flag = false;
+
+
+    public static void main(String[] args) {
+
+        Thread thread1 = new Thread(() -> printing(1));
+
+
+        Thread thread2 = new Thread(() -> printing(2));
+
+        thread1.start();
+        thread2.start();
+    }
+
+    private static void printing(int number) {
+        ThreadLocal<Integer> sThreadLocal = ThreadLocal.withInitial(()->number);
+        for (int i = 0; i < 5; i++) {
+            synchronized (lock) {
+                try {
+/*                    while ((number == 1 && flag) || (number == 2 && !flag)) {
+                        lock.wait();
+                        */
+
+/*  错误示范
+
+                   while (flag) {
+                        lock.wait();
+                    }*/
+
+
+/*
+ * flag作为状态切换 当状态切换之后当且仅当另外一个线程是可以进到代码块。
+ *   （1） 需要有一个线程的特定标识 来识别出当前线程
+ */
+                    while( (sThreadLocal.get()==1 && flag) || (sThreadLocal.get()==2 && !flag)){
+                        lock.wait();
+                    }
+                    System.out.print(number);
+                    flag = !flag;
+                    lock.notifyAll();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
+
+}
